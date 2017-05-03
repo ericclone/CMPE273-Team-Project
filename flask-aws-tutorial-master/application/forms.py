08656fa63a07ac7ef92ae97633ca32b51e6bac0d
Jiarui Hu <annhjr@gmail.com>
2017-05-03 15:44:15 -0700

    Update forms.py

--- flask-aws-tutorial-master/application/forms.py
+++ flask-aws-tutorial-master/application/forms.py
@@ -1,8 +1,24 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, validators
+from wtforms import TextField, validators,ValidationError
+
+def id_validator(form,field):
+    	checkid = field.data
+    	valid_chars ='0123456789'
+    	chars =list(checkid)
+    	for char in chars:
+    		if char not in valid_chars:
+    			raise ValidationError('Invalid SJSU ID')
 
 class EnterDBInfo(Form):
-    dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])    
+    dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), id_validator]) 
+    dbNotes2 = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')]) 
+    dbNotes4 = TextField(label='Email Address', description="db_enter", validators=[validators.required(), validators.Email()])   
+    dbNotes3 = TextField(label ='New Password',validators =[validators.required(),validators.EqualTo('confirm',message ='Passwords must match')])
+    confirm = TextField(label ='Repeat Password',validators =[validators.required()])  
+
+    
+             
 
 class RetrieveDBInfo(Form):
-    numRetrieve = TextField(label='Number of DB Items to Get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
+    userid = TextField(label='Items to add to DB', description="db_get", validators=[validators.required(), id_validator]) 
+    pw = TextField(label ='Password', description ="db_get", validators =[validators.required()]) 
