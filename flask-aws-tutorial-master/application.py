e4da4d6407127bd37324434cdc95ebf7fe23ca16
Jiarui Hu <annhjr@gmail.com>
2017-05-03 15:48:35 -0700

    Update application.py

--- flask-aws-tutorial-master/application.py
+++ flask-aws-tutorial-master/application.py
@@ -9,7 +9,7 @@ Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-
 
 from flask import Flask, render_template, request
 from application import db
-from application.models import Data
+from application.models import User
 from application.forms import EnterDBInfo, RetrieveDBInfo
 
 # Elastic Beanstalk initalization
@@ -25,26 +25,33 @@ def index():
     form2 = RetrieveDBInfo(request.form) 
     
     if request.method == 'POST' and form1.validate():
-        data_entered = Data(notes=form1.dbNotes.data)
+        data_entered = User(User_id=form1.dbNotes.data,Name=form1.dbNotes2.data,Type ='Student',Pw =form1.dbNotes3.data,Email = form1.dbNotes4.data)
+        
         try:     
             db.session.add(data_entered)
             db.session.commit()        
             db.session.close()
         except:
             db.session.rollback()
-        return render_template('thanks.html', notes=form1.dbNotes.data)
+        return render_template('login.html', studentid=form1.dbNotes.data)
         
     if request.method == 'POST' and form2.validate():
         try:   
-            num_return = int(form2.numRetrieve.data)
-            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-            for q in query_db:
-                print(q.notes)
+            userid = str(form2.userid.data)
+            password = str(form2.pw.data)
+            query_db = User.query.filter(User.User_id.in_([userid]),User.Pw.in_([password]))
+            result =query_db.first()
+            if result:
+                print "login successfully"
+                print result
+                return render_template('login.html',studentid =userid)
+            else:
+                print "login failed"
+                return render_template('login_failed.html',studentid =userid)
             db.session.close()
         except:
-            db.session.rollback()
-        return render_template('results.html', results=query_db, num_return=num_return)                
-    
+            db.session.rollback()      
+
     return render_template('index.html', form1=form1, form2=form2)
 
 if __name__ == '__main__':
