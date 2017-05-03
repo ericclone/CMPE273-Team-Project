7bf93a0f599422c3fe4ce2b4bd7e7d59bec3839c
Jiarui Hu <annhjr@gmail.com>
2017-05-03 15:45:30 -0700

    Update models.py

--- flask-aws-tutorial-master/application/models.py
+++ flask-aws-tutorial-master/application/models.py
@@ -1,11 +1,23 @@
 from application import db
 
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    notes = db.Column(db.String(128), index=True, unique=False)
+class User(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # notes = db.Column(db.String(128), index=True, unique=False)
+    __tablename__ = 'User'
     
-    def __init__(self, notes):
-        self.notes = notes
+    User_id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(128), index=True, unique=False)
+    Type = db.Column(db.String(128), index=True, unique=False)
+    Pw = db.Column(db.String(128), index=True, unique=False)
+    Email = db.Column(db.String(128), index=True, unique=False)
+    
+    def __init__(self, User_id,Name,Type,Pw,Email):
+        self.User_id = User_id
+        self.Name = Name
+        self.Type = Type
+        self.Pw = Pw
+        self.Email = Email
 
     def __repr__(self):
-        return '<Data %r>' % self.notes
+
+        return '<User %r>' % self.Name
