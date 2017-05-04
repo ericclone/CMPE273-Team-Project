51daa5c81f3885ed4ebefcdf1d9953a9047bf85d
Jiarui Hu <annhjr@gmail.com>
2017-05-03 20:11:31 -0700

    Update models.py

--- flask-aws-tutorial-master/application/models.py
+++ flask-aws-tutorial-master/application/models.py
@@ -21,3 +21,22 @@ class User(db.Model):
     def __repr__(self):
 
         return '<User %r>' % self.Name
+
+class Pre_req(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # notes = db.Column(db.String(128), index=True, unique=False)
+    __tablename__ = 'Pre_req'
+    
+    Pre_id = db.Column(db.Integer, primary_key=True)
+    Course = db.Column(db.String(128), index=True, unique=False)
+    Pre_course = db.Column(db.String(128), index=True, unique=False)
+    
+    def __init__(self, Pre_id,Course,Pre_course):
+        self.Pre_id = Pre_id
+        self.Course = Course
+        self.Pre_course = Pre_course
+       
+
+    def __repr__(self):
+
+        return '<Pre_req %r>' % self.Course
