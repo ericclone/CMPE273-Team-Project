7fb149603537cf1f6548940024650a53ef77050e
Qing Huang <hq1992518@msn.cn>
2017-05-07 15:55:20 -0700

    Update send_email to applicationFlask

--- flask-aws-tutorial-master/applicationFlask.py
+++ flask-aws-tutorial-master/applicationFlask.py
@@ -7,10 +7,20 @@ import base64
 import time
 import os
 from knn import knnTest
+from flask_mail import Mail,  Message
 
 application = Flask(__name__)
 # application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 application.secret_key = os.urandom(32)
+
+application.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'sjsu.cmpe273.test@gmail.com',
+    MAIL_PASSWORD = 'sjsu1234'
+)
+mail = Mail(application)
 
 '''
 Log in and sign up page form submit
@@ -127,6 +137,17 @@ def check_result():
     #Send mail    TODO
 
     return "page" #TODO
+
+@application.route("/send_mail", methods=['GET', 'POST'])
+def send_mail():
+    email = "hq1992518@gmail.com"
+    msg = mail.send_message(
+        'Hello',
+        sender='sjsu.cmpe273.test@gmail.com',
+        recipients=[email],
+        body="Testing"
+    )
+    return email
 
 
 def check_course_pre(taken_course_list, pre_list):
